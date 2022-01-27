209cf324f461e356f7cd1d8586c2f9227aa5c42e
xGREGKERSEYx <gregkkersey@gmail.com>
2022-01-26 20:24:12 -0500

    Push

---
+++ Tower of Terror/TowerOfTerror.java
@@ -0,0 +1,18 @@
+
+package towerofterror;
+
+import towerofterror.Floors;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class TowerOfTerror {
+
+    
+    public static void main(String[] args) {
+        
+        Floors floor = new Floors();
+        floor.startGame();
+
+    }
+    
+}
